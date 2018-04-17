b4bbd4fe9cc3975cb194ef502b0dc9b67322c442
Adam <abol@sas.upenn.edu>
2018-04-17 00:29:26 -0500

    add all ending or space method

--- shell/shell.c
+++ shell/shell.c
@@ -8,18 +8,21 @@
 
 #include "shell.h"
 
+#include "constants.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #include "job_parser.h"
 
 #define BUF_SIZE 514
 #define SHELL "520sh> "
 
-void print_error();
+void print_error(void);
 
 struct shell {
 	size_t runs;
@@ -30,6 +33,19 @@ shell_handle create_shell(void)
 	shell_handle new = (shell_handle)malloc(sizeof(*new));
 	new->runs = 0;
 	return new;
+}
+
+bool all_ending_or_space(char * string) {
+	size_t length = strlen(string);
+	for (int i = 0; i < length; i++)
+	{
+		char c = string[i];
+		//there needs to be something other than these terms
+		if (!isspace(c) && c != NULL_TERM && c != SEMICOLON && c != PLUS && c != PIPE && c != REDIRECT) {
+			return false;
+		}
+	}
+	return true;
 }
 
 void start_shell(shell_handle shell)
@@ -43,14 +59,15 @@ void start_shell(shell_handle shell)
 			print_error();
 			continue; // loop around again
 		}
+		if (all_ending_or_space(buffer)) {
+			continue;
+		}
 		job_parser_handle job_parser = create_job_parser(buffer);
-		commands_handle * commands = run_job_parser(job_parser);
-		if (commands == NULL) {
+		jobs_handle jobs = run_job_parser(job_parser);
+		if (jobs == NULL) {
 			print_error(); //input was incorrectly formatted
-			destroy_job_parser(job_parser);
 		}
 		destroy_job_parser(job_parser);
-		printf("\n");
 		(shell->runs)++;
 	}
 }
